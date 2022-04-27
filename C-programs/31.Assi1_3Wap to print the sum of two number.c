8c007a08e096046e05113e5fd32168bd27bd23d7
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-27 23:48:25 +0530

    print the sum of two numbers

---
+++ C-programs/31.Assi1_3Wap to print the sum of two number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,sum;
+	printf("enter two number");
+	scanf("%d%d",&num1,&num2);
+	sum=num1+num2;
+	printf("sum=%d",sum);
+	getch();
+}
