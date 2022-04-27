2457555451ac52781785f08379bde092c93bfa21
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-27 22:28:25 +0530

    formula of area of reactangle

---
+++ C-programs/13.rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int lenght,bridth,area;
+	printf("enter the lenght,bridth");
+	scanf("%d%d",&lenght,&bridth);
+	area=lenght*bridth;
+	printf("area=%d",area);
+	getch();
+	
+}
