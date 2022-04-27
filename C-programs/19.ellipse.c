68c339db594bfd3b8794fed6b9f190b942289ed9
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-27 23:31:17 +0530

    formula ofellipse

---
+++ C-programs/19.ellipse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float pi=3.142,area;
+	int a,b;
+	printf("enter the value of f a b");
+	scanf("%d%d",&a,&b);
+	area=a*b;
+	printf("area=%f",area);
+	getch();
+}
