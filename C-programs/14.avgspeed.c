d3ce6eebeb0829788c7b672a1ff590c8913e60ef
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-27 22:29:24 +0530

    formula of average speed

---
+++ C-programs/14.avgspeed.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int distance,time,S;
+	printf("enter the distance,time");
+	scanf("%d%d",&distance,&time);
+	S=distance/time;
+	printf("s=%d",S);
+	getch();
+}
