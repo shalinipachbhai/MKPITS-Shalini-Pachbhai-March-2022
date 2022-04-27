1f1106f813a550d7dab808e510b17466bae01fb8
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-27 23:37:06 +0530

    formula discharge Q

---
+++ C-programs/24.Discharge.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int velocity,area,Q;
+	printf("enter the value of velocity area");
+	scanf("%d%d",&velocity,&area);
+	Q=velocity*area;
+	printf("Q=%d",Q);
+	getch();
+	
+}
