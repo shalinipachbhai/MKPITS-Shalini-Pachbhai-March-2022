501b48aab062e30c76f01aa6ac56960b2a4f2072
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-27 23:49:45 +0530

    print  result of dividing two number

---
+++ C-programs/33.Assi1_5 Wap to print the result of dividing two numbers..c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,res;
+	printf("enter two number");
+	scanf("%d%d",&num1,&num2);
+	res=num1/num2;
+	printf("res=%d\n",res);
+	getch();
+}
