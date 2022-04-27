43ed26acb354ecd58c44b8c1e511c117c363e89e
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-27 23:55:02 +0530

    accepts two integers from the user

---
+++ C-programs/38.Assi1_10 Wap that accepts two integers from the user and calculate the sum of the two integers..c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n1,n2,sum;
+	printf("enter two integer");
+	scanf("%d%d",&n1,&n2);
+	sum=n1+n2;
+	printf("sum=%d\n",sum);
+	getch();
+
+}
