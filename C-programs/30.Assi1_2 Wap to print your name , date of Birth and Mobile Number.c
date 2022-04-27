b2e0731a9f1c275a6f55a09c53ecbf1b8930fb01
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-27 23:46:16 +0530

    Add files via upload
    print name , DOB and mobile number

---
+++ C-programs/30.Assi1_2 Wap to print your name , date of Birth and Mobile Number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int DOB;
+	int Mobile;
+	char Name;
+    printf("enter name DOB mobile number");
+	scanf("%c",&Name,&DOB,&Mobile);
+	printf("\n%c",Name);
+	printf("%d\n",DOB);
+	printf("%d\n",Mobile);
+	getch();
+}
