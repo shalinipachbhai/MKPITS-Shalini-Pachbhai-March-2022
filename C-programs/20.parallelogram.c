3042c14f3feacd780edc4cd58f89f92788d61ce5
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-27 23:32:22 +0530

    formula of arallelogram

---
+++ C-programs/20.parallelogram.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int base,height,area;
+	printf("enter the value of base height");
+	scanf("%d%d",&base,&height);
+	area=base*height;
+	printf("area=%d",area);
+	getch();
+}
