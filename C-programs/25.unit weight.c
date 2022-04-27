81b4c6766ea00f6f60bf459a6714b81213c936cb
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-27 23:37:45 +0530

    Add files via upload

---
+++ C-programs/25.unit weight.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int weight,volume,gyama;
+	printf("enter value of weight volume");
+	scanf("%d%d",&weight,&volume);
+	gyama=weight/volume;
+	printf("gyama=%d",gyama);
+	getch();
+}
