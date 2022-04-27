355f3612f6ff23fe6ab41bae54ffe0452a424075
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-27 23:34:30 +0530

    formula of speed

---
+++ C-programs/22.speed.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int distance,time,speed;
+	printf("enter the value of distance time");
+	scanf("%d%d",&distance,&time);
+	speed=distance/time;
+	printf("speed=%d",speed);
+	getch();
+	
+}
