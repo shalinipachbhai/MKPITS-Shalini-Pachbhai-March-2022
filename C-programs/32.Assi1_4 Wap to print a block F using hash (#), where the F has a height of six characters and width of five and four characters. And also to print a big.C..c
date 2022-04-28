f9731a5ae70ac251b317a32aa24a718f94881ef6
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-28 11:22:35 +0530

    print F and C

---
+++ C-programs/32.Assi1_4 Wap to print a block F using hash (#), where the F has a height of six characters and width of five and four characters. And also to print a big.C..c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("#####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#####\n");
+	printf("#\n");
+	printf("#\n");
+	
+	printf("   ######\n");
+	printf("##       ##\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("##       ##\n");
+	printf("    ######\n");
+	getch();
+}
