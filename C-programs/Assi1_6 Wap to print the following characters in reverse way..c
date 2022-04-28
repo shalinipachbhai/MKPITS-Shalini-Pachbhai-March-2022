7773dfa96b50e3c5089d42f5de2d1e7b87361b3f
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-28 11:26:17 +0530

    print characters in reverse way

---
+++ C-programs/Assi1_6 Wap to print the following characters in reverse way..c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char char1='X';
+	char char2='M';
+	char char3='L';
+	printf("the reverse of %c%c%cis%c%c%c\n",char1,char2,char3,char3,char2,char1);
+	getch();
+}
