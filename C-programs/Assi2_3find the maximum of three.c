9973e7e3ed091fb6344462183f239cc8da3a1940
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-28 11:32:38 +0530

    print maximum of three integers

---
+++ C-programs/Assi2_3find the maximum of three.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,num3,result,max;
+	printf("enter the three integers");
+	scanf("%d%d%d",&num1,&num2,&num3);
+	result=(num1+num2+(num1-num2)/2;
+	max=(result+num3+(result-num3))/2;
+	printf("maximum value of three integers=%d",max); 
+	getch();
+}
