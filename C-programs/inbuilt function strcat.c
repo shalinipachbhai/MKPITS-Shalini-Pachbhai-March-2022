fb3527486278bdd6bbdb72ca8158d044c5ab2f8c
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-28 12:37:34 +0530

    Add files via upload

---
+++ C-programs/inbuilt function strcat.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+
+	char name1[10]={"shalini"};
+	char name2[10]={"rutuja"};
+	printf("%s",strcat(name1,name2));
+
+	getch();
+}
