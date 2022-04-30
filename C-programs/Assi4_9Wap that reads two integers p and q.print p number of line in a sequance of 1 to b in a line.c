a07f0e09dccd1e79abe988db697e4e5241729e7d
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-30 06:19:31 +0530

    print p number of line in sequance of1to b in line

---
+++ C-programs/Assi4_9Wap that reads two integers p and q.print p number of line in a sequance of 1 to b in a line.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int x,y,i,j,l;
+	i=1;
+	printf("enter number of lines:\n");
+	scanf("%d",&x);
+	printf("enter number of lines:\n");
+	scanf("%d",&y);
+	for(i=1;i<=x;i++)
+	{
+	for(j=1;j<=y;j++)
+	{
+		printf("%d",l);
+		l++;
+	}
+	printf("\n");
+	}
+	getch();
+}
