e3cd13306fabda73b41fdc09a50b3a0a4a62ab2e
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-05-01 02:32:33 +0530

    Wap to print the result of the spcified operation.

---
+++ C-programs/Assi8_1 Wap to print the result of the spcified operation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r1,r2,r3,r4;
+	r1=-1+4*6;
+	printf("%d\n",r1);
+	r2=(35+5)%7;
+	printf("%d\n",r2);
+	r3=14+-4*6/11;
+	printf("%d\n",r3);
+	r4=2+15/6*1-7%2;
+	printf("%d\n",r4);
+	
+}
