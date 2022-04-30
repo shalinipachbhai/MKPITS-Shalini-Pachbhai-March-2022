1bc3319af320d6490b656e13c7494a05dc7c2948
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-30 06:27:50 +0530

    print the position and value less than 5

---
+++ C-programs/Assi5_6wap to read an array of length 5 and print the position and value less than 5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,a[5],n;
+	printf("enter five number");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&n);
+		
+	}
+	for(i=0;i<5;i++)
+	{
+		if(a[i]<5)
+		{
+			printf("a[%d]=%d\n",i,a[i]);
+		}
+	}
+}
