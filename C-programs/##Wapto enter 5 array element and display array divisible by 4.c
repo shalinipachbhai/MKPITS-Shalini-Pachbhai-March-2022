33c7c2ad3173c711b4100d542c24780364b6e141
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-04-30 06:10:20 +0530

    display array divisible by 4

---
+++ C-programs/##Wapto enter 5 array element and display array divisible by 4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a[5];
+printf("enter five numbers\n");
+for(i=0;i<4;i++)
+{
+	printf("a[%d]:",i);
+	scanf("%d",&a[i]);
+}
+printf("\n");
+for(i=0;i<4;i++)
+{
+	if(a[i]%4==0)
+	{
+	
+	printf("a[%d]=%d\n",i,a[i]);
+    }
+}
+getch();
+}
