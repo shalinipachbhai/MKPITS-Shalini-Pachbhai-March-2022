392d4377629282f179c1daca8643d1bc177f010c
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-05-01 02:33:28 +0530

    print the output of three numbers

---
+++ C-programs/Assi8_3 Wap to print the output of three numbers which will be  entered by the user.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float n1,n2,n3,mul;
+	printf("enter three number\n");
+	scanf("%f%f%f",&n1,&n2,&n3);
+	mul=n1*n2*n3;
+	printf("multiplication of three number: %f",mul);
+	
+}
