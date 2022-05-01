3a606b39bf7142af19aa81449735606681c048e2
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-05-01 15:21:10 +0530

    pattern 11

---
+++ C-programs/print pattern 11.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int r,c;
+	for(r=1;r<=4;r++)
+	{
+		for(c=1;c<=r;c++)
+		{
+		printf("*");
+        }
+		printf("\n");
+	}
+	for(r=3;r>=1;r--)
+	{
+		for(c=1;c<=r;c++)
+		{
+		printf("*");
+	    }
+	printf("\n");
+		
+	}
+}
