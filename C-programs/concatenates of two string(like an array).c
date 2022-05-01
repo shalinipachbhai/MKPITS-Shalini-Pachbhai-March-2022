2f22505995d57fbebb604475414142343dbe9db8
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-05-01 13:20:13 +0530

    concatenates of two string(like an array).

---
+++ C-programs/concatenates of two string(like an array).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i;
+	char ch1[20]={"abhijeet"};
+	char ch2[20]={"krishna"};
+	for(i=0;ch1[i]!='\0';i++)
+	{
+	printf("%c",ch1[i]);	
+	}
+	for(i=0;ch2[i]!='\0';i++)
+	{
+	printf("%c",ch2[i]);	
+	}	
+}
