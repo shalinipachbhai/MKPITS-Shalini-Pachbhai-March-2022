3e5219b30e5cc4ad7175296111f5b878b94c430d
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-05-12 12:28:37 +0530

    1pattern program

---
+++ C-programs/1pattern program.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	int r,c;
+	for(c=1;c<=r;c++)
+	{
+		if(c==1)
+		{
+		
+		printf("1");
+	    }
+	    else
+	    {
+	    	printf("0");
+		}
+	}
+	for(r=8;r>=1;r++)
+	{
+		if(c==1)
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}
+		
+	}
+	printf("\n");
+}
