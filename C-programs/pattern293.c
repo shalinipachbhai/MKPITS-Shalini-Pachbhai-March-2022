821d553fde9a207c866fe885e3d081b6063463a6
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-05-12 12:46:08 +0530

    pattern293

---
+++ C-programs/pattern293.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,ch=65;
+	for(i=5;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%c",ch);
+		}
+		
+		printf("\n");
+		ch=ch+1;
+	}
+}
