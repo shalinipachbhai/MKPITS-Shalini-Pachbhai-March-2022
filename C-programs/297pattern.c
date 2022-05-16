7271d1887a7b8475004a968a7963a7e162fda600
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-05-16 14:21:02 +0530

    297pattern

---
+++ C-programs/297pattern.c
@@ -0,0 +1,26 @@
+//   1
+//  12
+// 123
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c,add=0;
+	for(r=1;r<=3;r++)
+	{
+		for(c=1;c<=3;c++)
+		{
+			if(r+c>=4)
+			{
+				add=add+1;
+				printf("%d",add);
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		add=0;
+		printf("\n");
+	}
+}
