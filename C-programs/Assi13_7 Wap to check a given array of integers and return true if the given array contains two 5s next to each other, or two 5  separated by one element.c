0b2102f1a3a0443e683da4589fe13c72933b81b7
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-05-18 12:43:53 +0530

    Wap to check a given array of integers and return true if the given array contains two 5s next to each other, or two 5 separated by one element

---
+++ C-programs/Assi13_7 Wap to check a given array of integers and return true if the given array contains two 5s next to each other, or two 5  separated by one element.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,a[10],size,f=0;
+	printf("enter the size of array: ");
+	scanf("%d",&size);
+	printf("enter array element\n");
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<size;i++)
+	{
+		if(a[i]==5&&(a[i+1]==5||a[i+2]==5))
+		{
+			f=1;
+		}
+	}
+	printf("\n");
+	if(f==1)
+	{
+		printf("true");
+	}
+	else
+	{
+		printf("false");
+		
+	}
+	
+}
