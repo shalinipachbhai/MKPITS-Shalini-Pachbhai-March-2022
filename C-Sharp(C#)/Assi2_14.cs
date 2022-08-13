4504dbdf82bc0d350afe04ae210ef33940d58838
shalinipachbhai <102147054+shalinipachbhai@users.noreply.github.com>
2022-08-12 23:51:10 -0700

    Add files via upload

---
+++ C-Sharp(C#)/Assi2_14.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2._14
+{
+    internal classtatic 
+        
+    {
+        void Main(string[])
+        {
+        // wap to accept ch aracter form the user and display whether it is vowel or not.(by using if else if)
+       char ch;
+       Console.WriteLine("Enter Character");
+       ch=Convert.ToChar(Console.ReadLine());
+       if(ch=='a'|| ch=='A')
+       {
+       Console.WriteLine("Enter character is vowel");
+       }
+       else if(ch=='e'||ch=='E')
+       {
+        Console.WriteLine("Enter character is vowel");
+       }
+       else if(ch=='i')
+       {
+        Console.WriteLine("Enter Character is vowel");
+       }
+       else if(ch=='o')
+       {
+        Console.WriteLine("Enter Character is vowel");
+       }
+       else if(ch=='u')
+       {
+        Console.WriteLine("Enter Character is vowel");
+       }
+       else
+       {
+        Console.WriteLine("Enter Character is not vowel");
+       }
+        }
+    }
+}
